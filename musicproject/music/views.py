ca3b851491d2b8aa437132db4f0da56fcfd4c204
codymart1986 <codymart1986@hotmail.com>
2021-08-08 12:12:08 -0700

    added song detail api view with functions for get by id, put, delete. added a function to update in serializers. added path in url patterns to the song detail view

--- musicproject/music/views.py
+++ musicproject/music/views.py
@@ -4,6 +4,7 @@ from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 # Create your views here.
 class SongList(APIView):
@@ -19,3 +20,29 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+    
+    def get_song(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        song = self.get_song(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        song = self.get_song(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valide():
+            serializer.save
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_song(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
